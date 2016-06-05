64875dd59dd63a40c513a70c8ac4449631e0803b
Mac <mac@MAcs-iMac-2.local>
2016-06-05 15:45:52 +0300

    Add json support

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,29 @@ router.get('/', function(req, res, next) {
         if (err)
             console.error(err);
         else {
-            console.log(json);
+            //console.log(json);
             //var jsonData = JSON.parse(json);
-            console.log(json[0].doc_title);
+            //console.log(json[0].doc_title);
 
             res.render('index', {
-                title: json[0].doc_title,
-                car_prof: "Some"
+                title: json.jm_doc_title,
+                jm_user_mail: json.jm_user_mail,
+                jm_user_phone: json.jm_user_phone,
+                jm_user_in: json.jm_user_in,
+                jm_user_website: json.jm_user_website,
+                jm_user_website_vs : json.jm_user_website_vs,
+                jm_user_github: json.jm_user_github,
+                jm_user_twitter: json.jm_user_twitter,
+                jm_username: json.jm_username,
+                jm_user_title: json.jm_user_title,
+                jm_interests:json.jm_interests,
+                jm_languages:json.jm_languages,
+                jm_educations: json.jm_educations,
+                jm_skills: json.jm_skills,
+                jm_career_profile_summary: json.jm_career_profile_summary,
+                jm_experiences: json.jm_experiences,
+                jm_project_intro: json.jm_project_intro,
+                jm_projects: json.jm_projects
             });
 
             //jsonUtil.writeJson(JSON_DATA, jsonData, function (err) {
